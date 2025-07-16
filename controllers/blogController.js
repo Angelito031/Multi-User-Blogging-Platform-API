0051b30d028cbb8473166f8a98ed266235b3943b
Angelito Casas <angelitocasas23@gmail.com>
2025-07-16 16:29:33 +0800

    Added Update, DeleteById and Delete all Blog Post

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -35,7 +35,7 @@ export const GetBlogById = async (req, res) => {
     }
 
     const query =
-      "SELECT blogs.bid, blogs.title, blogs.content, blogs.created_at, (users.firstname users.lastname) AS author_name FROM blogs LEFT JOIN users ON blogs.author_id = users.uid WHERE blogs.bid = $1";
+      "SELECT blogs.bid, blogs.title, blogs.content, blogs.created_at, (users.firstname, users.lastname) AS author_name FROM blogs LEFT JOIN users ON blogs.author_id = users.uid WHERE blogs.bid = $1";
 
     const result = await client.query(query, [id]);
 
@@ -88,5 +88,105 @@ export const CreateBlogPost = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Internal server error has occured." });
+  }
+};
+
+export const UpdateBlogPost = async (req, res) => {
+  try {
+    const roleId = req.user?.role_id;
+    const { blogId } = req.params;
+    const { title, content } = req.body;
+
+    if (roleId !== 1 || roleId !== 2) {
+      return res
+        .status(403)
+        .json({ message: "Sorry only ADMIN and EDITOR can edit blog post." });
+    }
+
+    const checkBlogPostIfExistQuery =
+      "SELECT blogs.title, blogs.content, blog.authord_id WHERE blogs.bid = $1";
+
+    const { rows } = await client.query(checkBlogPostIfExistQuery, [blogId]);
+
+    if (rows.length === 0) {
+      return res.status(200).json({ message: "Blog Post not exist." });
+    }
+
+    const updateBlogPost =
+      "UPDATE blogs SET title = $1, content = $2, author_id = $3 WHERE blogs.bid = $4";
+
+    const updateResult = await client.query(updateBlogPost, [
+      title,
+      content,
+      rows[0].author_id,
+    ]);
+
+    if (updateResult.rows.length === 0) {
+      return res.status(200).json({
+        message: "An error has occured while updating the blog post.",
+      });
+    }
+
+    return res.status(200).json({ message: "Blog post updated successfully." });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error has occured." });
+  }
+};
+
+export const DeleteBlogPost = async (req, res) => {
+  try {
+    const { confirm } = req.body;
+    const { blogId } = req.params;
+    const isAdmin = req.user?.role_id === 1; //ADMIN ROLE ID
+
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Only admins are allowed to delete all blogs.." });
+    }
+
+    if (!confirm) {
+      return res
+        .status(400)
+        .json({ message: "Confirmation required to delete all blogs." });
+    }
+
+    const query = "DELETE FROM blogs WHERE blogs.bid = $1";
+    await client.query(query, [blogId]);
+
+    return res.status(200).json({ message: "Deleted a blog post." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const DeleteAllBlogs = async (req, res) => {
+  try {
+    const { confirm } = req.body;
+    const isAdmin = req.user?.role_id === 1; //ADMIN ROLE ID
+
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Only admins are allowed to delete all blogs.." });
+    }
+
+    if (!confirm) {
+      return res
+        .status(400)
+        .json({ message: "Confirmation required to delete all blogs." });
+    }
+
+    const query = "DELETE FROM blogs";
+    await client.query(query);
+
+    return res.status(200).json({ message: "Deleted all blogs." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,17 +1,24 @@
 import express from "express";
 import { isAuthenticated, isRoleAllowed } from "../middleware/auth.js";
-import { GetAllBlogs, GetBlogById } from "../controllers/blogController.js";
+import {
+  CreateBlogPost,
+  DeleteAllBlogs,
+  DeleteBlogPost,
+  GetAllBlogs,
+  GetBlogById,
+  UpdateBlogPost,
+} from "../controllers/blogController.js";
 
 const BlogRoutes = express.Router();
 
 BlogRoutes.route("/")
   .get(isAuthenticated, isRoleAllowed("admin", "editor", "reader"), GetAllBlogs)
-  .post(isAuthenticated, isRoleAllowed("admin", "editor"))
-  .delete(isAuthenticated, isRoleAllowed("admin", "editor"));
+  .post(isAuthenticated, isRoleAllowed("admin", "editor"), CreateBlogPost)
+  .delete(isAuthenticated, isRoleAllowed("admin", "editor"), DeleteAllBlogs);
 
 BlogRoutes.route("/:id")
   .get(isAuthenticated, isRoleAllowed("admin", "editor", "reader"), GetBlogById)
-  .put(isAuthenticated, isRoleAllowed("admin", "editor"))
-  .delete(isAuthenticated, isRoleAllowed("admin", "editor"));
+  .put(isAuthenticated, isRoleAllowed("admin", "editor"), UpdateBlogPost)
+  .delete(isAuthenticated, isRoleAllowed("admin", "editor"), DeleteBlogPost);
 
 export default BlogRoutes;
